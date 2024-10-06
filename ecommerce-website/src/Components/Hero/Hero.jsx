658b174ab8dba860714909b04e444596579dacb4
chmueedzafar <chmueedzafar@gmail.com>
2024-10-06 23:23:14 +0500

    modify commit

--- ecommerce-website/src/Components/Hero/Hero.jsx
+++ ecommerce-website/src/Components/Hero/Hero.jsx
@@ -1,32 +1,39 @@
-import React from 'react'
-import './Hero.css'
-import hand_icon from '../Assets/hand-img.png'
-import arrow_icion from '../Assets/arrow-img.png'
-import hero_image from '../Assets/girl-img.jpg'
+import React from 'react';
+import './Hero.css';
+import handIcon from '../Assets/hand-img.png';
+import arrowIcon from '../Assets/arrow-img.png';
+import heroImage from '../Assets/girl-img.jpg';
+
 const Hero = () => {
   return (
-    <div className='hero'>
-      <div className='hero-left'>
-      <h2>NEW ARRIVALS ONLY</h2>
-      <div>
-      <div className='hero-hand-icon'>
-        <p>new</p>
-        <img src={hand_icon} alt="hand icon" className='hand-img'/>
-      </div>
-      <p>collections</p>
-      <p>for everyone</p>
-      </div>
-      <div className='hero-latest-btn'>
-        <div>Latest Collection</div>
-        <img src={arrow_icion} alt="arrow-icion" className='arrow-img' />
-      </div>
-      </div>
-      <div className='hero-right'>
-        <img src={hero_image} alt="hero-image" />
+    <div className="hero">
+      <div className="hero-content">
+        {/* Left Side - Text and Icons */}
+        <div className="hero-left">
+          <h2>NEW ARRIVALS ONLY</h2>
 
+          <div className="hero-info">
+            <div className="hero-hand-icon">
+              <p className="new-tag">new</p>
+              <img src={handIcon} alt="Hand pointing to new collections" className="hand-img" />
+            </div>
+            <p className="hero-tagline">Collections</p>
+            <p className="hero-tagline">For Everyone</p>
+          </div>
+
+          <div className="hero-latest-btn">
+            <div className="latest-btn-text">Latest Collection</div>
+            <img src={arrowIcon} alt="Arrow pointing to the latest collection" className="arrow-img" />
+          </div>
+        </div>
+
+        {/* Right Side - Hero Image */}
+        <div className="hero-right">
+          <img src={heroImage} alt="Fashionable woman showcasing the latest collection" className="hero-img" loading="lazy" />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
