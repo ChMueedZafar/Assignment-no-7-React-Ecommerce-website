9f2a3e916b9c94eb962bf82501bd3f72968cd274
chmueedzafar <chmueedzafar@gmail.com>
2024-10-09 00:27:48 +0500

    hero section end

--- ecommerce-website/src/Components/Hero/Hero.jsx
+++ ecommerce-website/src/Components/Hero/Hero.jsx
@@ -1,14 +1,33 @@
 import React from 'react';
 import './Hero.css';
-// import handIcon from '../Assets/hand-img.png';
-// import arrowIcon from '../Assets/arrow-img.png';
-// import heroImage from '../Assets/girl-img.jpg';
+import handicon from '../Assets/hand-img.png'
+import arrowIcon from '../Assets/arrow-img.png';
+import heroImage from '../Assets/girl-img-removebg-preview.png';
 
 const Hero = () => {
 
 
   return (
-   <div></div>
+   <div className='hero'>
+    <div className='hero-left'>
+      <h2>NEW ARRIVALS ONLY</h2>
+      <div>
+        <div className='hero-hand-icon'>
+        <p>new</p>
+        <img  src={handicon} alt="hand icon img" className='handimg'/>
+        </div>
+        <p>Collections</p>
+        <p>for everyone</p>
+      </div>
+      <div className='hero-latest-btn'>
+        <div>Latest Collection</div>
+         <img src={arrowIcon} alt="arrowimg" className='arrowimg' />
+      </div>
+    </div>
+    <div className='hero-right'>
+      <img src={heroImage} alt="hero-img" className='heroimg' />
+    </div>
+   </div>
   );
 };
 
